9dda9f63e310f88bcec9fc4e955219e0d56a87ad
Kenrique100 <ngwakenri2016@gmail.com>
2026-02-19 00:27:30 +0100

    remove duplicate our pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -334,24 +334,6 @@ export default async function Home() {
                             </CardContent>
                         </Card>
 
-                        {/* Pages Card - Dynamic from DB */}
-                        <Card className="border border-gray-200 dark:border-gray-800 bg-white dark:bg-gray-950 rounded-xl">
-                            <CardHeader className="pb-2">
-                                <div className="flex items-center justify-between">
-                                    <CardTitle className="text-sm font-semibold text-gray-900 dark:text-gray-100 flex items-center gap-2">
-                                        <FileTextIcon className="w-4 h-4 text-purple-600" />
-                                        Our Pages
-                                    </CardTitle>
-                                    <Link href="/pages" className="text-[11px] text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300 hover:underline">
-                                        View All
-                                    </Link>
-                                </div>
-                            </CardHeader>
-                            <CardContent className="pt-0">
-                                <PagesDropdown pages={JSON.parse(JSON.stringify(dynamicPages))} />
-                            </CardContent>
-                        </Card>
-
                         {/* About Us */}
                         <Card className="border border-gray-200 dark:border-gray-800 bg-white dark:bg-gray-950 rounded-xl">
                             <CardHeader className="pb-2">
